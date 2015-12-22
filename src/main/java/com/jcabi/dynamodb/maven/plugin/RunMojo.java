2c1bb0dfdc2cb70fab5a08c25269727c6058f019
Denis N. Antonioli <denisa@sunrunhome.com>
2015-12-22 09:00:19 -0800

    Line length

--- src/main/java/com/jcabi/dynamodb/maven/plugin/RunMojo.java
+++ src/main/java/com/jcabi/dynamodb/maven/plugin/RunMojo.java
@@ -41,8 +41,8 @@ import org.apache.maven.plugins.annotations.Parameter;
  * Starts DynamoDB Local.
  *
  * @todo #41:30min Let's avoid code duplication between this class and `StartMojo`.
- *  One idea is to  create a decorator called `ThreadedMojo` that receives another
- *  mojo in its constructor and, when called, runs it inside a thread
+ *  One idea is to create a decorator called `ThreadedMojo` that receives
+ *  another mojo in its constructor and, when called, runs it inside a thread.
  *
  * @author Denis N. Antonioli (denisa@sunrun.com)
  * @version $Id$
